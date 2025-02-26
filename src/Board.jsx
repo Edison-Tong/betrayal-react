083a548fcf37a6d601f57c0b39d3c99dd6a5e2a4
Edison-Tong <edisonjtong@gmail.com>
2025-02-25 18:22:38 -0700

    board renders well

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,12 +1,21 @@
 import Tile from "./Tiles";
 import tilesData from "./tilesData";
 
-export default function Board({ className, tiles = [] }) {
+export default function Board({ className, tiles = [], size }) {
+  const [rows, cols] = size;
+
   return (
-    <div className={className}>
+    <div
+      className={className}
+      style={{
+        display: "grid",
+        gridTemplateRows: `repeat(${rows}, 1fr)`,
+        gridTemplateColumns: `repeat(${cols}, 1fr)`,
+      }}
+    >
       {tiles.map((tileId, index) => {
-        const tileData = tilesData.find((tile) => tile.id === tileId); // Find the matching tile data
-        return tileData ? <Tile key={index} data={tileData} /> : null;
+        const tileData = tilesData.find((tile) => tile.id === tileId);
+        return tileData ? <Tile key={index} data={tileData} row={tileData.row} col={tileData.col} /> : null;
       })}
     </div>
   );
--- src/Game.jsx
+++ src/Game.jsx
@@ -3,9 +3,13 @@ import Board from "./Board";
 export default function Game() {
   return (
     <div className="game-table">
-      <Board className="board basement" tiles={["basement-landing"]} />
-      <Board className="board ground active" tiles={["entrance-hall", "hallway", "ground-floor-staircase"]} />
-      <Board className="board upper" tiles={["upper-landing"]} />
+      {/* <Board className="board basement" size={[5, 5]} tiles={["basement-landing"]} /> */}
+      <Board
+        className="board ground active"
+        size={[5, 5]}
+        tiles={["entrance-hall", "hallway", "ground-floor-staircase"]}
+      />
+      {/* <Board className="board upper" size={[5, 5]} tiles={["upper-landing"]} /> */}
     </div>
   );
 }
